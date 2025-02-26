3f192d3508c7828306d6d125793331df1d32bde6
hpeter19 <njoshpitar@gmail.com>
2025-02-26 17:48:52 +0300

    python dictionaries

---
+++ test.py
@@ -0,0 +1,3 @@
+student = {'name' :'paul', 'age': 25 }
+del student['age']
+print (student)
---
+++ test2.py
@@ -0,0 +1,6 @@
+#program to pop out age
+
+student= {'name':'john','age':25 ,'course':['math','art']}
+age = student.pop('age')
+print(student  )
+print(age)
